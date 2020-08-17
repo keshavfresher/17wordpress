a3a99b20aee233a7fc8b209359188e2c8d88b01d
michaelbrinkworth <michaelhireitem@gmail.com>
2020-08-17 17:30:46 +1000

    Themes
    Themes

---
+++ listeo-child/listeo-core/archive/no-found.php
@@ -0,0 +1,13 @@
+<div id="listeo-listings-container">
+							<div class="loader-ajax-container" style=""> <div class="loader-ajax"></div> </div>
+<section id="listings-not-found" class="margin-bottom-50 col-md-12">
+	<h2><?php esc_html_e('Partners are still being listed','listeo_core'); ?></h2>
+	<!--<p><?php // _e( 'We&rsquo;re sorry but we do not have any listings matching your search, try to change you search settings', 'listeo_core' ); ?></p>-->
+	<!-- <p><?php  _e( 'We&rsquo;re sorry, there are Hypley partners in this area but are still being onboarded. Please contact your event details and what you need to', 'listeo_core' ); ?> <a target="_blank" href="mailto:justin@hypley.com">justin@hypley.com</a> </p> -->
+	<p>We're sorry, there are Hypley partners in this area still being listed. Please email your event details of what you need to <a target="_blank" href="mailto:justine@hypley.com"> justine@hypley.com </a> Then <a href="<?php echo site_url(); ?>/my-profile/"> sign up </a> and sit back while Hypley partners send you the best deals.</p>
+</section>
+</div>
+<div class="not_found_modal">
+    <iframe class="not_found_iframe" src="https://form.jotform.co/m_manley-nahu/hypleyy" title="W3Schools Free Online Web Tutorials">
+    </iframe>
+</div>
